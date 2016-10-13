d476b786670809393f892c27abd9eedf21615037
Ante Mišković <ante.miskovic@vsite.hr>
2016-10-13 20:15:38 +0200

    Dovrsili smo .NET framework

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -3,10 +3,16 @@
 namespace HelloWorld
 {
     class Program
+
     {
         static void Main(string[] args)
         {
+            int a = 14;
+            double b = 3.14;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
             Console.WriteLine("Zdravo");
+            Console.ReadKey();
         }
     }
 }
